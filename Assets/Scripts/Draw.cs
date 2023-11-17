a8d6263a8eee73adbc3d46ac132c1cac47adf02f
nuh-yilmaz <nuh.yilmaz@istanbul.edu.tr>
2023-11-17 15:53:22 +0300

    All-in-One Code

---
+++ Assets/Scripts/Draw.cs
@@ -0,0 +1,180 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Runtime.InteropServices;
+using Oculus.Interaction.PoseDetection;
+
+public class Manager : MonoBehaviour
+{
+
+    private OVRHand m_hand;
+    private OVRSkeleton m_skeleton;
+    private Transform indexTip;
+    private Transform indexDistal;
+    [SerializeField, Range(0f, 0.5f)]
+    private float distanceOffset;
+    private const int WHITEBOARD_LAYER = 10;
+    private LineRenderer lineRenderer;
+    private bool isDrawing = false;
+
+    private int errorCount = 0;  // Added errorCount variable
+    public Text errorCountText;
+    public GameObject SquareIn;  // Reference to SquareIn object
+    public GameObject SquareOut; // Reference to SquareOut object
+    public GameObject Paper; 
+
+    private float startTime;
+    private float totalElapsedTime = 0f;
+    public Text elapsedTimeText;
+
+    private int PortAddress;
+    private int Data;
+    [DllImport("inpoutx64.dll")]
+    private static extern void Out32(int PortAddress, int Data);
+    [DllImport("inpoutx64.dll")]
+    private static extern UInt32 IsInpOutDriverOpen();
+
+    void Start()
+    {
+        // Create an empty GameObject for LineRenderer
+        GameObject lineObject = new GameObject("LineRendererObject");
+        lineRenderer = lineObject.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 0; // Start with an empty line
+        lineRenderer.startWidth = 0.01f; // Adjust the width of the line
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.Simplify(10);
+        lineRenderer.material = new Material(Shader.Find("Standard"));
+        lineRenderer.material.color = Color.black; // Set the line color to black
+
+        // Assign your parallel port address (LPT3)
+        PortAddress = 0x278; // LPT3 address
+
+        // Ensure SquareIn, SquareOut, and Paper are assigned in the Unity Editor
+        if (SquareIn == null || SquareOut == null || Paper == null)
+        {
+            Debug.LogError("Please assign SquareIn, SquareOut, and Paper in the Unity Editor.");
+            return;
+        }
+    }
+
+    public void Awake()
+    {
+        // Get the scripts that hold information about hand tracking
+        m_hand = GetComponent<OVRHand>();
+        m_skeleton = GetComponent<OVRSkeleton>();
+    }
+
+    void FixedUpdate()
+    {
+        if (indexTip == null && m_skeleton.IsInitialized)
+        {
+            Debug.Log("Skeleton initialized");
+            indexTip = m_skeleton.Bones[(int)OVRPlugin.BoneId.Hand_IndexTip].Transform;
+            indexDistal = m_skeleton.Bones[(int)OVRPlugin.BoneId.Hand_Index3].Transform;
+        }
+
+        // If hands aren't initialized yet, don't execute the rest of the script.
+        if (!indexTip) return;
+
+        Vector3 originPoint = indexDistal.position;
+        Vector3 targetPoint = indexTip.position;
+
+        Vector3 direction = Vector3.Normalize(targetPoint - originPoint);
+        float distance = Vector3.Distance(originPoint, targetPoint);
+
+        // Cast a ray starting from the second index finger joint to the tip of the index finger.
+        // Only check for objects that are in the whiteboard layer.
+        if (Physics.Raycast(originPoint, direction, out RaycastHit touch, distance + distanceOffset, 1 << WHITEBOARD_LAYER) ||
+            Physics.Raycast(targetPoint, -direction, out touch, distance + distanceOffset, 1 << WHITEBOARD_LAYER))
+        {
+            if (touch.collider != null)
+            {
+                // Check if the touch is within the borders of SquareOut
+                if (IsWithinBorders(touch.point, SquareOut))
+                {
+                    // Check if the touch is outside the borders of SquareIn
+                    if (!IsWithinBorders(touch.point, SquareIn))
+                    {
+                        // Increment error count
+                        errorCount++;
+                        SendTriggerToParallelPort(2);
+
+                        // Display error count on UI Text
+                        if (errorCountText != null)
+                        {
+                            errorCountText.text = "Hata: " + errorCount.ToString();
+                        }
+                    }
+                }
+
+                if (!isDrawing)
+                {
+                    // Start drawing when touched
+                    isDrawing = true;
+                    startTime = Time.time;
+
+                    SendTriggerToParallelPort(1);
+
+                    lineRenderer.positionCount = 1;
+                    lineRenderer.SetPosition(0, touch.point);
+                }
+                else
+                {
+                    // Update LineRenderer positions when drawing
+                    lineRenderer.positionCount++;
+                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, touch.point);
+                }
+               
+
+                // Optional: Rotate the lineRenderer to match the hand rotation
+                Vector3 handUp = m_hand.transform.up;
+                Vector3 worldUp = Vector3.up;
+                Quaternion rotation = Quaternion.FromToRotation(handUp, worldUp);
+                lineRenderer.transform.rotation = rotation * Quaternion.identity;
+
+                // Display elapsed time in the scene (optional)
+                if (elapsedTimeText != null)
+                {
+                    totalElapsedTime += Time.deltaTime;
+                    elapsedTimeText.text = "Elapsed Time: " + totalElapsedTime.ToString("F2") + " seconds";
+                }
+            }
+        }
+        else
+        {
+            // Stop drawing when not touching
+            isDrawing = false;
+        }
+    }
+    private void SendTriggerToParallelPort(int triggerValue)
+    {
+        // Set the Data value to the unique trigger value for each error
+        Data = triggerValue;
+        Out32(PortAddress, Data);
+
+        // Reset the trigger immediately
+        Data = 0;
+        Out32(PortAddress, Data);
+    }
+
+    private bool IsWithinBorders(Vector3 point, GameObject square)
+    {
+        // Check if the point is within the borders of the square
+        Renderer squareRenderer = square.GetComponent<Renderer>();
+        Bounds squareBounds = squareRenderer.bounds;
+
+        return squareBounds.Contains(point);
+    }
+    private void CenterSquareOnPaper(GameObject square)
+    {
+        // Ensure the square and paper are not null
+        if (square == null || Paper == null)
+        {
+            return;
+        }
+
+        // Set the position of the square to the center of the paper
+        square.transform.position = Paper.transform.position;
+    }
+}
--- Assets/Scripts/HandDraw.cs
+++ Assets/Scripts/HandDraw.cs
@@ -10,40 +10,17 @@ public class HandDraw : MonoBehaviour
     private OVRHand m_hand;
     private OVRSkeleton m_skeleton;
 
-    private Transform indexTip;
-    private Transform indexDistal;
+    private Transform indexTip, indexDistal;
 
-    [SerializeField]
-    private Vector3 originPoint;
-    private Vector3 targetPoint;
-    private Vector3 direction;
-    private float distance;
-    [SerializeField, Range(0f, 0.5f)]
-    private float distanceOffset;
-    public bool isTouching;
-    private const int WHITEBOARD_LAYER = 10;
-    private const int ERROR_LAYER = 8;
-    private const string ERROR_TAG = "Error";
-
-    private int PortAddress;
-    private int Data;
-
-    [DllImport("inpoutx64.dll")]
-    private static extern void Out32(int PortAddress, int Data);
-
-    [DllImport("inpoutx64.dll")]
-    private static extern UInt32 IsInpOutDriverOpen();
-
-    // Counter variables
-    private int errorHitCount = 0;
-    public TimeDisplay timeDisplay;  // Reference to the TimeDisplay script
+    [SerializeField] private Vector3 originPoint, targetPoint, direction;
+    [SerializeField, Range(0f, 0.5f)] private float distanceOffset;
+    [SerializeField] private LayerMask layermask;
 
     public void Awake()
     {
         // Get the scripts that hold information about hand tracking
         m_hand = GetComponent<OVRHand>();
         m_skeleton = GetComponent<OVRSkeleton>();
-        timeDisplay = FindAnyObjectByType<TimeDisplay>();  // Find the TimeDisplay script in the scene
     }
 
     // Update is called once per frame
@@ -63,18 +40,16 @@ public class HandDraw : MonoBehaviour
         targetPoint = indexTip.position;
 
         direction = Vector3.Normalize(targetPoint - originPoint);
-        distance = Vector3.Distance(originPoint, targetPoint);
+        float distance = Vector3.Distance(originPoint, targetPoint);
         distanceOffset = 0.0015f;
 
         // Cast a ray starting from the second index finger joint to the tip of the index finger.
         // Only check for objects that are in the whiteboard layer.
-        if (Physics.Raycast(originPoint, direction, out touch, distance + distanceOffset, 1 << WHITEBOARD_LAYER) ||
-            Physics.Raycast(targetPoint, -direction, out touch, distance + distanceOffset, 1 << WHITEBOARD_LAYER))
+        if (Physics.Raycast(originPoint, direction, out touch, distance + distanceOffset, layermask) || Physics.Raycast(targetPoint, -direction, out touch, distance + distanceOffset, layermask))
         {
             if (m_hand.GetFingerConfidence(OVRHand.HandFinger.Index) != OVRHand.TrackingConfidence.High)
                 return;
 
-            // indexTip.position = touch.point;
             // Get the Whiteboard component of the whiteboard we obtain from the raycast.
             whiteboard = touch.collider.GetComponent<WhiteboardForHand>();
 
@@ -85,16 +60,7 @@ public class HandDraw : MonoBehaviour
 
             // If the raycast intersects the board, it means we are touching the board
             whiteboard.ToggleTouch(true);
-            isTouching = true;
-            timeDisplay.UpdateTime();
 
-            if (touch.collider.CompareTag(ERROR_TAG))
-            {
-                errorHitCount++;
-
-                // Update the UI or send a signal as needed
-                UpdateCountAndSignal();
-            }
         }
         else
         {
@@ -102,30 +68,7 @@ public class HandDraw : MonoBehaviour
             {
                 // If the raycast no longer intersects, stop drawing on the board.
                 whiteboard.ToggleTouch(false);
-                isTouching = false;
-
-                // Stop the stopwatch when not touching the whiteboard
-                timeDisplay.StopTime();
             }
         }
-    }
-    void UpdateCountAndSignal()
-    {
-        // Update the UI text or send a signal using the specified function
-        // Replace this with your actual UI update or signal-sending logic
-        Debug.Log("Error Hit Count: " + errorHitCount);
-
-        // Send the signal
-        SendSignal();
-    }
-
-    void SendSignal()
-    {
-        // Send the signal using the specified function (Out32 in this case)
-        // Replace this with your actual signal-sending logic
-        Out32(20220, 2);
-
-        // Reset the signal
-        Out32(20220, 0);
     }
 }
